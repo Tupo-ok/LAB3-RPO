1ad5135a72fcb6246e3b09ce3c56ebafead73ff9
Tupo-ok <kep202@inbox.ru>
2023-03-18 19:57:32 +0300

    initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,31 +1,107 @@
 package com.example.myapplication;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.myapplication.databinding.ActivityMainBinding;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 
-public class MainActivity extends AppCompatActivity {
+interface TransactionEvents {
+    String enterPin(int ptc, String amount);
+    void transactionResult(boolean result);
+}
+
+public class MainActivity extends AppCompatActivity implements TransactionEvents {
 
     // Used to load the 'myapplication' library on application startup.
     static {
         System.loadLibrary("myapplication");
+        System.loadLibrary("mbedcrypto");
     }
 
-    private ActivityMainBinding binding;
+    private String pin;
+    ActivityResultLauncher activityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+
+        setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = binding.sampleText;
+        TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        // initialization of mbedcrypto component
+        int init = initRng();
+
+        Button btn = (Button) findViewById(R.id.button);
+
+        activityResultLauncher  = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            assert data != null;
+                            pin = data.getStringExtra("pin");
+                            synchronized (MainActivity.this) {
+                                MainActivity.this.notifyAll();
+                            }
+                            // Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+
+
+        btn.setOnClickListener(view -> {
+            new Thread(()-> {
+                try {
+                    byte[] trd = stringToHex("9F0206000000000100");
+                    boolean ok = transaction(trd);
+                } catch (Exception ex) {
+                    // todo: log error
+                }
+            }).start();
+        });
+
+    }
+
+    @Override
+    public void transactionResult(boolean result) {
+        runOnUiThread(()-> {
+            Toast.makeText(MainActivity.this, result ? "ok" : "failed", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    public String enterPin(int ptc, String amount) {
+        pin = new String();
+        Intent it = new Intent(MainActivity.this, PinpadActivity.class);
+        it.putExtra("ptc", ptc);
+        it.putExtra("amount", amount);
+        synchronized (MainActivity.this) {
+            activityResultLauncher.launch(it);
+            try {
+                MainActivity.this.wait();
+            } catch (Exception ex) {
+                //todo: log error
+            }
+        }
+        return pin;
     }
 
     /**
@@ -33,4 +109,19 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+
+    public static native byte[] encrypt (byte[] key, byte [] array);
+
+    public static native byte[] decrypt (byte[] key, byte[] array);
+
+    public static byte[] stringToHex(String s) {
+        byte[] hex;
+        try { hex = Hex.decodeHex(s.toCharArray()); }
+        catch (DecoderException ex) { hex = null; }
+        return hex;
+    }
+
+    public native boolean transaction(byte[] trd);
 }
